7aebe8a2db320be9840e356e7014d51042ef5fce
hanashiro <felipeaah@gmail.com>
2020-06-08 20:05:49 -0300

    aula-03-02

---
+++ app/data/services/ApiService.js
@@ -0,0 +1,18 @@
+const api = 'http://192.168.0.28:3002/api/treinafood/';
+
+export const ApiService = {
+    get(endpoint){
+        return fetch(`${api}${endpoint}`)
+            .then(response => response.json())
+    },
+    post(endpoint, data){
+        return fetch(`${api}${endpoint}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+    }
+}
---
+++ app/data/services/NumberService.js
@@ -0,0 +1,9 @@
+export const NumberService = {
+    currency(value){
+        const formatter =  new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        })
+        return formatter.format(value);
+    }
+}
