cb2a85327fcb9d85d30aa21074ec76234686db01
hanashiro <felipeaah@gmail.com>
2020-06-08 12:19:42 -0300

    aula-02-02

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+    ...DefaultTheme,
+    dark: true,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: '#a91f1f'
+    }
+}
+
+
+export default function Main(){
+    return (
+        <PaperProvider theme={theme} >
+            <App />
+        </PaperProvider>
+    )
+}
+
+AppRegistry.registerComponent(appName, () => Main);
